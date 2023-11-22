515199fe6cfc7336f84ceabc8a5418ad505370c1
vanshajjaroraa <148533435+vanshajjaroraa@users.noreply.github.com>
2023-11-21 16:18:42 -0800

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,90 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image2":
+            case "myImage":return img`
+. . . . . . . . . . . . . . . . 
+. . 3 3 3 3 3 3 3 . . . . . . . 
+. 3 3 . 3 3 3 3 3 3 . . . . . . 
+. 3 . . 3 3 3 3 3 3 . . . . . . 
+. 3 . . 3 3 3 3 . 3 . . . . . . 
+. 3 . 3 3 3 3 3 . 3 . . . . . . 
+. 3 3 3 . 3 . 3 . 3 . . . . . . 
+. . . . . 3 3 3 3 . . . . . . . 
+. . . . . . 3 3 3 . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image3":
+            case "myImage0":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image1":
+            case "Peter Smith":return img`
+111111111111111111111111
+111119..1..1......11...1
+...11999...c11..11111111
+.....11....ccc11........
+.......ccc.999ccccc.....
+..99999ccc9cc599995cc...
+.....339c9b5999999555c..
+.....cc399999999955555c.
+.....cc9b5999999f155555c
+......c999999955ff55d55c
+......99999995995555555c
+...cc.95999995599b13bbc.
+...cc99999dd9d559b3335c.
+.....9999999dddd95b335c.
+..ccc9ddddb55bbdd95555c.
+..ccc9ddddb555bbb9cccc..
+...ccd99dddb5555c9cdc...
+ccccbdd99dd5cb999bcc....
+cdddddddd99999ccbbc.....
+.cddddddbdd555bbbcc.....
+..ccdddbbbdd55cbcdc.....
+....ccbbcbddddccdddcc...
+......cccdd555dcccccc...
+........cccccccc........
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
---
+++ main.ts
@@ -0,0 +1,1 @@
+let mySprite = sprites.create(assets.image`Peter Smith`, SpriteKind.Player)
